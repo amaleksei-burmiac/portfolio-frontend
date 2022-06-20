2f9ae22e07f7dfbac31884d972a0ded03464c18b
amaleksei-burmiac <zh.burmiac@gmail.com>
2022-06-21 07:42:08 +0800

    [Update] обновлена главная страница

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react'
 import styles from '../../styles/Home.module.css'
+import Characters from '../components/characters/characters'
 import Header from '../components/header'
+import { ICharacter } from '../interfaces/characters'
+import getWorks from '../lib/work'
 
-function Home() {
+export async function getServerSideProps() {
+  const charactersRes = await getWorks()
+  const characters = charactersRes?.data?.results
+
+  return {
+    props: {
+      characters,
+    },
+  }
+}
+
+function Home({ characters }: { characters: ICharacter[] }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,35 +29,7 @@ function Home() {
       <main className={styles.main}>
         <Header />
         <h1 className={styles.title}>Hello World!</h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href='https://nextjs.org/docs' className={styles.card}>
-            <h2>Documentation &rarr</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href='https://nextjs.org/learn' className={styles.card}>
-            <h2>Learn &rarr</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href='https://github.com/vercel/next.js/tree/master/examples' className={styles.card}>
-            <h2>Examples &rarr</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-            className={styles.card}
-          >
-            <h2>Deploy &rarr</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <Characters characters={characters} />
       </main>
 
       <footer className={styles.footer}>
